a34a701586548fe196d29ab1ad8f1b2ac808e5bd
toni-dev <62844491+Toni-d-e-v@users.noreply.github.com>
2021-01-10 18:02:15 +0100

    Update cli.go

--- cli/cli.go
+++ cli/cli.go
@@ -8,9 +8,9 @@ import (
 	"runtime"
 	"strconv"
 
-	"github.com/tensor-programming/golang-blockchain/blockchain"
-	"github.com/tensor-programming/golang-blockchain/network"
-	"github.com/tensor-programming/golang-blockchain/wallet"
+	"github.com/Redstone-2-0/core-in-go/blockchain"
+	"github.com/Redstone-2-0/core-in-go/network"
+	"github.com/Redstone-2-0/core-in-go/wallet"
 )
 
 type CommandLine struct{}
